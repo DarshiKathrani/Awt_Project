b69737d8f74654c734b69bb0c6979a84a8c7c112
DarshiKathrani <darshikathrani4@gmail.com>
2026-03-12 09:00:42 +0530

    Final backend changes for  deployment

--- Lab11/auth-app/src/app.module.ts
+++ Lab11/auth-app/src/app.module.ts
@@ -1,25 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { MeetingsController} from './meetings/meetings.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
+  import { Module } from '@nestjs/common';
+  import { AuthModule } from './auth/auth.module';
+  import { MeetingsController} from './meetings/meetings.controller';
+  import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({
-  imports: [AuthModule
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'mysql-9cda2c3-darshikathrani4-5f8c.e.aivencloud.com',
-    //   port: 15262,
-    //   username: 'avnadmin',
-    //   password: 'AVNS_C9Nl0jeVKQ6hZpcv5xP',
-    //   database: 'defaultdb',
-    //   ssl: {
-    //     rejectUnauthorized: false
-    //   },
-    //   autoLoadEntities: true,
-    //   synchronize: false
-    // }),
-  ],
-  controllers:[MeetingsController]
-  
-})
-export class AppModule {}
+  @Module({
+    imports: [AuthModule
+      // TypeOrmModule.forRoot({
+      //   type: 'mysql',
+      //   host: 'mysql-9cda2c3-darshikathrani4-5f8c.e.aivencloud.com',
+      //   port: 15262,
+      //   username: 'avnadmin',
+      //   password: 'AVNS_C9Nl0jeVKQ6hZpcv5xP',
+      //   database: 'defaultdb',
+      //   ssl: {
+      //     rejectUnauthorized: false
+      //   },
+      //   autoLoadEntities: true,
+      //   synchronize: false
+      // }),
+    ],
+    controllers:[MeetingsController]
+    
+  })
+  export class AppModule {}
--- Lab11/auth-app/src/main.ts
+++ Lab11/auth-app/src/main.ts
@@ -1,20 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
-import { JwtService } from '@nestjs/jwt';
-import { AdminGuard } from './auth/admin.guard';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser()); 
-   
-   app.enableCors({
-    origin: 'http://localhost:3001', 
-    credentials: true,           
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3001', 
+      process.env.FRONTEND_URL   
+    ],
+    credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
