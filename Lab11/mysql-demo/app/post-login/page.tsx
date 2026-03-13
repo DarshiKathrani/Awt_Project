ec22f5f8a8aa5d28f0ed3cdcb4055221f20b5df5
DarshiKathrani <darshikathrani4@gmail.com>
2026-03-13 16:12:37 +0530

    changes in post login

--- Lab11/mysql-demo/app/post-login/page.tsx
+++ Lab11/mysql-demo/app/post-login/page.tsx
@@ -1,3 +1,28 @@
-export default function PostLogin() {
-  return null; 
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import jwt from "jsonwebtoken";
+
+export default async function PostLogin() {
+
+  const token = (await cookies()).get("token")?.value;
+
+  if (!token) {
+    redirect("/");
+  }
+
+  const decoded: any = jwt.decode(token);
+
+  if (decoded.role === "admin") {
+    redirect("/admin");
+  }
+
+  if (decoded.role === "staff") {
+    redirect("/staff");
+  }
+
+  if (decoded.role === "meeting_convener") {
+    redirect("/convener-dashboard");
+  }
+
+  redirect("/");
 }
