88421a26bf80730bb3d0cd2b4f294099962fb612
DarshiKathrani <darshikathrani4@gmail.com>
2026-03-13 19:51:45 +0530

    disable middleware

--- Lab11/mysql-demo/middleware.ts
+++ Lab11/mysql-demo/middleware.ts
@@ -1,27 +1,27 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get('token')?.value;
-  const pathname = req.nextUrl.pathname;
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get('token')?.value;
+//   const pathname = req.nextUrl.pathname;
 
-  // allow public pages
-  if (pathname === '/' || pathname === '/not-authorized' || pathname === '/post-login') {
-    return NextResponse.next();
-  }
+//   // allow public pages
+//   if (pathname === '/' || pathname === '/not-authorized' || pathname === '/post-login') {
+//     return NextResponse.next();
+//   }
 
-  // protect dashboard routes
-  if (!token) {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
+//   // protect dashboard routes
+//   if (!token) {
+//     return NextResponse.redirect(new URL('/', req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: [
+// export const config = {
+//   matcher: [
 
-    '/admin/:path*',
-    '/dashboard/:path*',
-    '/convener-dashboard/:path*'
-  ],
-};
+//     '/admin/:path*',
+//     '/dashboard/:path*',
+//     '/convener-dashboard/:path*'
+//   ],
+// };
