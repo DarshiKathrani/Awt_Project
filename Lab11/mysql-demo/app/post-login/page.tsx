6f845d561f75a3a4b1e94c1e038411f16095a191
DarshiKathrani <darshikathrani4@gmail.com>
2026-03-13 16:29:38 +0530

    Final changes 3

--- Lab11/mysql-demo/app/post-login/page.tsx
+++ Lab11/mysql-demo/app/post-login/page.tsx
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function PostLogin() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
