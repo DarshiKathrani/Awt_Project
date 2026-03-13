5f3cd9e41cd30080e5906140bd453ad4d7d48c0b
DarshiKathrani <darshikathrani4@gmail.com>
2026-03-13 16:35:36 +0530

    Final changes 4

--- Lab11/mysql-demo/middleware.ts
+++ Lab11/mysql-demo/middleware.ts
@@ -19,7 +19,7 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    '/post-login',
+
     '/admin/:path*',
     '/dashboard/:path*',
     '/convener-dashboard/:path*'
