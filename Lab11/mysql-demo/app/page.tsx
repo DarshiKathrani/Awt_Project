fd3b8b98676f18b2d4fb1f97390affc98822be47
DarshiKathrani <darshikathrani4@gmail.com>
2026-03-13 15:46:48 +0530

    Final login changes

--- Lab11/mysql-demo/app/page.tsx
+++ Lab11/mysql-demo/app/page.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
     setMessage('');
 
     try {
-      const res = await fetch('http://localhost:3000/auth/login', {
+      const res = await fetch('http://awt-project-glqp.onrender.com/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
--- Lab11/mysql-demo/lib/db.ts
+++ Lab11/mysql-demo/lib/db.ts
@@ -15,12 +15,12 @@
 // lib/db.ts
 import mysql from 'mysql2/promise';
 
-export const pool = await mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "MeetingManagementDB",
-  password: "qwef@#12",
-});
+// export const pool = await mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "MeetingManagementDB",
+//   password: "qwef@#12",
+// });
 
 // export async function db() {
 //   const [rows] = await connection.query("select * from MeetingType");
