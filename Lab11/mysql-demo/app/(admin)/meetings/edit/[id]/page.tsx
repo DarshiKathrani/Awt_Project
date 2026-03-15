b01629b1c0b1b43b3feb4bcf2b8377c3e33554bc
DarshiKathrani <darshikathrani4@gmail.com>
2026-03-15 09:03:20 +0530

    edit form changes

--- Lab11/mysql-demo/app/(admin)/meetings/edit/[id]/page.tsx
+++ Lab11/mysql-demo/app/(admin)/meetings/edit/[id]/page.tsx
@@ -2,19 +2,19 @@ import { prisma } from "@/lib/prisma";
 import EditMeetingForm from "./EditMeetingForm";
 
 export default async function EditMeetingPage({ params }: { params: { id: string } }) {
+  const { id } = await params;
   // 1. Convert the ID from string (from URL) to Number
-  const meetingId = Number(params.id);
+  const meetingId = Number(id);
 
   // 2. Query exactly using the column name defined in your schema
-  const meeting = await prisma.meetings.findUnique({
-    where: {
-      MeetingID: meetingId,
-    },
-  });
-
-  // 3. Fetch types
-  const meetingTypes = await prisma.meetingtype.findMany();
-
+  const [meeting, meetingTypes] = await Promise.all([
+    prisma.meetings.findFirst({ 
+      where: { 
+        MeetingID: meetingId 
+      } 
+    }),
+    prisma.meetingtype.findMany(),
+  ]);
   if (!meeting) {
     return <div>Meeting not found! (ID provided: {params.id})</div>;
   }
